e094eda095ac6bb1d9f490d1435723dfc693b412
Jacob <791262675@qq.com>
2019-12-03 17:54:53 +0800

    初创一个SpringBoot项目，上传到gitHub

---
+++ src/main/java/com/funtl/my/shop/SpringBootMyShopApplication.java
@@ -0,0 +1,11 @@
+package com.funtl.my.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyShopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMyShopApplication.class, args);
+    }
+}
